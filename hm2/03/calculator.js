f03c7cd37b007d51c0f4831a9005174cef8cf101
Andrew <andrey.shemiakov@gmail.com>
2016-11-10 13:59:42 +0300

    ДЗ 2-3 Оформление через module.exports

--- hm2/03/calculator.js
+++ hm2/03/calculator.js
@@ -34,9 +34,4 @@ function calculator(firstNumber) {
     }
 }
 
-var myCalculator = calculator(100);
-
-console.log(myCalculator.sum(1, 2, 3)); //вернет 106
-console.log(myCalculator.dif(10, 20)); //вернет 70
-console.log(myCalculator.div(2, 2)); //вернет 25
-console.log(myCalculator.mul(2, 2)); //вернет 400
+module.exports = calculator;
