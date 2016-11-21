f8e6e01a4f8afa804f49f8be1ad1804aba35b438
Andrew <andrey.shemiakov@gmail.com>
2016-11-22 00:09:28 +0300

    ДЗ 5-2

---
+++ hm5/02/dragAndDrop.js
@@ -0,0 +1,65 @@
+let container = document.querySelector('.container'),
+    button = document.querySelector('.button'),
+    minSizeValue = 50,
+    maxSizeValue = 500,
+    minCoordValue = 0,
+    maxCoordValue = 500,
+    lastElem;
+
+function getRandomValueByRange(minValue, maxValue) {
+    return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+}
+
+function getRandomColor() {
+    let red = getRandomValueByRange(0, 255),
+        green = getRandomValueByRange(0, 255),
+        blue = getRandomValueByRange(0, 255);
+
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function dragAndDrop() {
+    if (lastElem) {
+        lastElem.remove();
+    }
+
+    let width = getRandomValueByRange(minSizeValue, maxSizeValue),
+        height = getRandomValueByRange(minSizeValue, maxSizeValue),
+        left = getRandomValueByRange(minCoordValue, maxCoordValue),
+        top = getRandomValueByRange(minCoordValue, maxCoordValue),
+        color = getRandomColor(),
+        randomRect = document.createElement('div'),
+        currentElem = false,
+        startX = 0,
+        startY = 0;
+
+    randomRect.style.position = 'absolute';
+    randomRect.style.width = width + 'px';
+    randomRect.style.height = height + 'px';
+    randomRect.style.left = left + 'px';
+    randomRect.style.top = top + 'px';
+    randomRect.style.backgroundColor = color;
+
+    container.appendChild(randomRect);
+
+    randomRect.addEventListener('mousedown', function (e) {
+        currentElem = e.target;
+        startY = e.layerY;
+        startX = e.layerX;
+    });
+
+    randomRect.addEventListener('mouseup', function () {
+        currentElem = undefined;
+    });
+
+    randomRect.addEventListener('mousemove', function (e) {
+        if (currentElem) {
+            randomRect.style.top = e.clientY - startY + 'px';
+            randomRect.style.left = e.clientX - startX + 'px';
+        }
+    });
+
+    lastElem = randomRect;
+}
+
+button.addEventListener('click', dragAndDrop);
